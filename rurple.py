f9dfbf4c78f0b32ad6a32be7118ae3c480cb61ac
jose123456879 <totitosst@gmail.com>
2016-08-24 08:43:55 -0600

    add

--- rurple.py
+++ rurple.py
@@ -2,8 +2,10 @@ from cargar_mapa import cargar_mapa
 
 Mapa1=("mapa.txt")
 Mapa2=("mapa1.txt")
-
-opc=int(input("ingrese el mapa deseado:\n1. mapa1 \n2.mapa2"))
+for numero in linea:
+	if numero=="0"
+	print(" " end="")
+opc=int(input("ingrese el mapa deseado:\n1. mapa1 \n2.mapa2 \n"))
 if opc==1:
 	print(cargar_mapa(Mapa1))
 
