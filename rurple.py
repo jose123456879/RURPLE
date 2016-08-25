1bdd219d09a2a71a48ff0e78efa40fa0b7db9676
jose123456879 <totitosst@gmail.com>
2016-08-25 08:43:25 -0600

    sadd

--- rurple.py
+++ rurple.py
@@ -5,8 +5,9 @@ Mapa2=("mapa1.txt")
 for numero in linea:
 	if numero=="0"
 	print(" " end="")
-opc=int(input("ingrese el mapa deseado:\n1. mapa1 \n2.mapa2 \n"))
-if opc==1:
+print("ingrese el numero del mapa deseado:\n1. mapa1\n 2.mapa2")	
+opc=int(input("ingrese el numero: "))
+	if opc==1:
 	print(cargar_mapa(Mapa1))
 
 	
