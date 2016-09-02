63264197c35deb2c7da1295792fbde76566779dd
jose123456879 <totitosst@gmail.com>
2016-09-02 08:07:14 -0600

    changes

--- mapa.py
+++ mapa.py
@@ -1,9 +1,29 @@
-def cargar_mapa(f):
-	mapa=open(f, "r")
-	lista=[]
-	for linea in mapa:
-		lista.append(list(linea))
-	return lista
+declass Mapa(object):
+	def __init__(self, altura, ancho, fichas, robot):
+		self.altura = altura
+		self.ancho = ancho
+		self.fichas = []
+		self.robot = None
 
-Mapa=("mapa.txt")
-print(cargar_mapa(Mapa))	
+	def dibujar(self):
+		resultado = ""
+		for y in range(self.altura):
+			for x in range(self.ancho):
+				if ficha.x == x and ficha.y == y:
+					resultado += ficha
+				elif robot.x == x and robot.y == y:
+					resultado += robot
+				else:
+					resultado += " "
+			resultado += "\n"
+
+	def contar_fichas(self,x,y):
+		conteo = 0
+		for y in range(self.altura):
+			for x in range(self.ancho):
+				if ficha.y == y and ficha.x == x:
+					conteo += ficha
+		return conteo
+
+	def agregar_ficha(self,x,y):
+		self.ficha.append(ficha)
