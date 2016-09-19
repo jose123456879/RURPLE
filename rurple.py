65a3e7b6255d2ab2e447947b73122c859ea1fcff
jose123456879 <totitosst@gmail.com>
2016-09-19 08:43:02 -0600

    vsd

--- rurple.py
+++ rurple.py
@@ -4,26 +4,21 @@ import time
 
 def cargar_mapa(nombre):
 	name= open(nombre, "r")
-	lista_mapa = []
-	robot = Robot(0,0)
-	for linea in name:
-		lista.append(list(linea))
-	return lista	
+	lista=[]
+	for i in Mapa:
+		lista.append(list(i.strip()))
+	Mapa.close()	
+	return lista
 def cargar_instrucciones(nombre):
-	archivo=open(nombre,"r")
-	lista_intrucciones=[]
-	for i in intrucciones:
+	name=open(nombre,"r")
+	instrucciones=[]
+	for i in instrucciones:
 		instrucciones.append(i.strip())
 
-Mapa1=("mapa.txt")
-Mapa2=("mapa1.txt")
-
-print("ingrese el numero del mapa deseado:\n1. mapa1\n2. imapa2")	
-opc=int(input("ingrese el numero: "))
-if opc==1:
-	print(cargar_mapa(Mapa1))
-	print(cargar_instrucciones)
-
+nombre_mapa=input("ingrese el nobre del archivo: ")
+nombre_instrucciones=input("ingrese las instrucciones: ")
+lista_mapa=cargar_mapa(nombre_mapa)
+nombre_instrucciones=cargar_instrucciones(nombre_instrucciones)
 objeto_mapa = Mapa((len(lista_mapa[0])) , (len(lista_mapa))) 
 
 for x in range(len(lista_mapa)):
@@ -36,6 +31,19 @@ for x in range(len(lista_mapa)):
 			for k in range(int(lista_mapa[x][y])):
 				objeto_moneda = Monedas(y , x)
 				objeto_mapa.asignar_moneda(objeto_moneda)
+print(objeto_mapa.didujar())
+
+
+for x in lista_intrucciones:
+	if x =="rotar":
+		objeto_robot.rotar()
+
+	if x=="mover":
+		objeto_robot.mover()
+
+	if x=="recojer":
+		objeto_robot.recojer()
+
 
 print("\n")
 print("monedas restantes: " + objeto_mapa.monedas_en_mapa())
@@ -43,5 +51,8 @@ print("monedas obtenidas: " + objeto_robot.monedas)
 print(objeto_mapa.dibujar())
 print("\n")
 
+
+print(objeto_mapa.dibujar())
+time.sleep(0.4)
 
 
